dc079bf2a88e7026f5e317072a78db40a75d829b
earpjennings <wjford28@gmail.com>
2026-03-06 15:31:15 -0500

    helm edits

---
+++ helm.tf
@@ -0,0 +1,22 @@
+##############################
+# HELM PROVIDER — EAST
+##############################
+provider "helm" {
+  alias = "east"
+  kubernetes {
+    host                   = module.eks_east.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_east.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.east.token
+  }
+}
+##############################
+# HELM PROVIDER — WEST (optional)
+##############################
+provider "helm" {
+  alias = "west"
+  kubernetes {
+    host                   = var.enable_west ? module.eks_west[0].cluster_endpoint : ""
+    cluster_ca_certificate = var.enable_west ? base64decode(module.eks_west[0].cluster_certificate_authority_data) : ""
+    token                  = var.enable_west ? data.aws_eks_cluster_auth.west[0].token : ""
+  }
+}
