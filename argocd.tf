8121a9e5f87d3e3e3c31a56cd1e5afda87e101b3
earpjennings <wjford28@gmail.com>
2026-08-10 15:22:41 -0400

    version # change

--- argocd.tf
+++ argocd.tf
@@ -27,7 +27,7 @@ resource "helm_release" "argocd_west" {
   namespace        = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "6.7.12"
+  version          = "7.1.0"
   create_namespace = true
   values = [
     file("${path.module}/helm-charts/argocd/values.yaml")
