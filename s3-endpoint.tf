19baa7ef373bff7510a2caf03e8b933ef24dcb58
earpjennings <wjford28@gmail.com>
2026-08-17 21:15:34 -0400

    allow S3 endpoint access for container registries

--- s3-endpoint.tf
+++ s3-endpoint.tf
@@ -1,26 +1,3 @@
-data "aws_iam_policy_document" "s3_endpoint" {
-  statement {
-    sid    = "AllowThanosBucket"
-    effect = "Allow"
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-
-    actions = [
-      "s3:ListBucket",
-      "s3:GetObject",
-      "s3:PutObject"
-    ]
-
-    resources = [
-      "arn:aws:s3:::thanos-infinity-37",
-      "arn:aws:s3:::thanos-infinity-37/*"
-    ]
-  }
-}
-
 resource "aws_vpc_endpoint" "s3_east" {
   provider          = aws.east
   vpc_id            = module.vpc_east.vpc_id
@@ -28,8 +5,6 @@ resource "aws_vpc_endpoint" "s3_east" {
   vpc_endpoint_type = "Gateway"
 
   route_table_ids = module.vpc_east.public_route_table_ids
-
-  policy = data.aws_iam_policy_document.s3_endpoint.json
 
   tags = merge(
     local.tags,
