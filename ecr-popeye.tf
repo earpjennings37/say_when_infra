3bfacaa66f4b53f2f108f1ed7dde7575b5e43675
earpjennings <wjford28@gmail.com>
2026-08-26 12:19:06 -0400

    allow ECR repository force delete

--- ecr-popeye.tf
+++ ecr-popeye.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "popeye" {
   name                 = "popeye-arm64"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
