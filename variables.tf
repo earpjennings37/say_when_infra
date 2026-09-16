c5c7d4e9a56c12142ace61a5c4c40c1564271f6f
earpjennings <wjford28@gmail.com>
2026-09-16 15:31:31 -0400

    broaden EKS Spot instance types

--- variables.tf
+++ variables.tf
@@ -62,7 +62,7 @@ variable "cluster_version" {
 variable "node_instance_types" {
   description = "Instance types for EKS managed node groups (SPOT fallback list)"
   type        = list(string)
-  default     = ["t4g.medium"]
+  default     = ["t4g.medium", "c6g.large", "c7g.large"]
 }
 
 variable "node_min_size" {
